12928d06c46600c1bcc770aa046c5d61dacbd0ff
Josh Hurn <joshua.hurn@hotmail.com>
2022-02-15 15:24:03 -0600

    fin pt.2

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
-let friendsArr = ["Josh", "Garrett", "Haylee", "Haley", "Haleigh"];
+const singBtn = document.querySelector("#sing-btn");
 
-for (let i = 0; i < friendsArr.length; i++) {
-    console.log(friendsArr[i].toUpperCase() + ":");
+singBtn.addEventListener("click", function () {
+    let friendsArr = ["Josh", "Garrett", "Haylee", "Haley", "Haleigh"];
 
-    for (let j = 99; j > 0; j--) {
-        if (j == 2) {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArr[i] + " strikes one out, clears it all out, 1 line of code in the file");
-        } else if (j == 1) {
-            console.log(j + " line of code in the file, " + j + " line of code; " + friendsArr[i] + " strikes one out, clears it all out, no more lines of code in the file");
-        } else {
-            console.log(j + " lines of code in the file, " + j + " lines of code; " + friendsArr[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file");
+    for (let i = 0; i < friendsArr.length; i++) {
+        let friendDiv = document.createElement("div");
+        friendDiv.className = "friend";
+
+        let friendH3 = document.createElement("h3");
+        friendH3.innerText = friendsArr[i].toUpperCase();
+
+        friendDiv.appendChild(friendH3);
+
+        for (let j = 99; j > 0; j--) {
+            let paragraph = document.createElement("p");
+
+            if (j == 2) {
+                paragraph.innerText = j + " lines of code in the file, " + j + " lines of code; " + friendsArr[i] + " strikes one out, clears it all out, 1 line of code in the file"
+            } else if (j == 1) {
+                paragraph.innerText = j + " line of code in the file, " + j + " line of code; " + friendsArr[i] + " strikes one out, clears it all out, no more lines of code in the file"
+            } else {
+                paragraph.innerText = j + " lines of code in the file, " + j + " lines of code; " + friendsArr[i] + " strikes one out, clears it all out, " + (j - 1) + " lines of code in the file"
+            }
+
+            friendDiv.appendChild(paragraph);
         }
+
+        document.body.appendChild(friendDiv);
     }
-} 
+});
